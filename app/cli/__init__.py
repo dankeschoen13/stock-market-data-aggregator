4f881cc2d1d923c9e8b778408c73763bb2f95458
dankeschoen13 <marcobernacer@gmail.com>
2026-05-31 21:32:31 +0800

    feat: implement extraction of FMP's API key from flask app configuration instead of directly from env variables.

---
+++ app/cli/__init__.py
@@ -0,0 +1,1 @@
+from .cli import register_cli_commands
--- app/cli/cli.py
+++ app/cli/cli.py
@@ -1,8 +1,9 @@
-from flask.cli import with_appcontext
 from app.services import MktDataSvc, TickerSvc, fetch_latest_stock_data
 from app.constants import TickerSets
+from flask.cli import with_appcontext
+from flask import current_app
 import pandas as pd
-import click, os
+import click
 
 
 @click.command(name='get-stock-data')
@@ -12,25 +13,36 @@ def get_stock_data_command():
     Fetches daily data for all active tickers and loads it into the database.
     """
 
-    tickers = TickerSvc.get_active_tickers()
+    # 1.A | DATA PREP: Get the api key.
+    api_key = current_app.config.get('FMP_KEY')
+    if not api_key:
+        click.secho(
+            "Critical Error: FMP_KEY is missing from app configuration.",
+            fg="red", err=True
+        )
+        return
 
+    # 1.B | DATA PREP: Get the list of tracked tickers.
+    tickers = TickerSvc.get_active_tickers()
     if not tickers:
         click.echo("No active tickers found in the database to update.")
         return
 
+
+    # 2. | MAIN PROCESS: Loop through the ticker list, fetch data and save to db.
     click.echo(f"Starting market data update for {len(tickers)} tickers...")
 
     for symbol in tickers:
         click.echo(f"Fetching data for {symbol.ticker}...")
 
         try:
-            # 1. Extract
-            stock_data = fetch_latest_stock_data(symbol.ticker, os.environ.get('FMP_KEY'))
+            # a. Extract
+            stock_data = fetch_latest_stock_data(symbol.ticker, api_key)
 
-            # 2. Validate
+            # b. Validate
             if isinstance(stock_data, pd.DataFrame):
 
-            # 3. Load
+            # c. Load
                 MktDataSvc.load_data(stock_data)
                 click.secho(
                     f"  ✔ Success! {symbol.ticker} data updated.",
@@ -43,8 +55,11 @@ def get_stock_data_command():
                 )
 
         except Exception as e:
-            # Catch unexpected extraction network errors or database SQLAlchemyErrors
-            click.secho(f"  ! Critical error processing {symbol.ticker}: {e}", fg="red", err=True)
+            # d. Catch unexpected extraction network errors or database SQLAlchemyErrors
+            click.secho(
+                f"  ! Critical error processing {symbol.ticker}: {e}",
+                fg="red", err=True
+            )
 
     click.echo("Market data update complete!")
 
@@ -56,7 +71,7 @@ def seed_tickers_command():
     Seeds the TrackedTicker table with an initial batch of popular stocks.
     """
 
-    initial_tickers = TickerSets.SET_B
+    initial_tickers = TickerSets.SET_C
     added_count = 0
 
     # 1. Loop through the list of initial tickers and add them to session
@@ -72,12 +87,18 @@ def seed_tickers_command():
         success, error_msg = TickerSvc.save_changes()
 
         if success:
-            click.echo(f"Success! Seeded {added_count} new tickers into the database.")
+            click.echo(
+                f"Success! Seeded {added_count} new tickers into the database."
+            )
         else:
-            click.echo(f"Database error during commit: {error_msg}",
-                       err=True)
+            click.echo(
+                f"Database error during commit: {error_msg}",
+                err=True
+            )
     else:
-        click.echo("All tickers are already present in the database. No changes made.")
+        click.echo(
+            "All tickers are already present in the database. No changes made."
+        )
 
 def register_cli_commands(app):
     """
