e07b4da36fe7d1b5f5d91284ca91b60b1abe4bd2
dankeschoen13 <marcobernacer@gmail.com>
2026-06-02 10:53:38 +0800

    feat: add dynamic JSON serialization method to Stock model
    - Implement a `to_dict` instance method on the PostgreSQL `Stock` model to dynamically map `__table__.columns` to dictionary keys.
    - Add strict type checking within the loop to automatically convert `datetime` objects to ISO 8601 strings and `Decimal` values to floats, ensuring safe parsing by Flask's `jsonify`.
    - Exclude `id` column from data dict to minimize payload weight

--- app/models/models.py
+++ app/models/models.py
@@ -1,8 +1,10 @@
-from datetime import date
 from sqlalchemy import String, Date, Float, BigInteger, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import expression
 from app.extensions import db
+from decimal import Decimal
+from datetime import date
+import datetime
 
 
 class Stock(db.Model):
@@ -29,6 +31,33 @@ class Stock(db.Model):
     rsi_14: Mapped[float | None] = mapped_column(Float, nullable=True)
     macd_line: Mapped[float | None] = mapped_column(Float, nullable=True)
     ema_50: Mapped[float | None] = mapped_column(Float, nullable=True)
+
+
+    def to_dict(self):
+
+        data = {}
+        excluded_cols = ['id']
+
+        for column in self.__table__.columns:
+
+            value = getattr(self, column.name)
+
+            if column.name in excluded_cols:
+                continue
+
+            # Dates and Datetimes => ISO 8601 strings
+            if isinstance(value, (datetime.date, datetime.datetime)):
+                data[column.name] = value.isoformat() if value else None
+
+            # Decimals => floats
+            elif isinstance(value, Decimal):
+                data[column.name] = float(value) if value is not None else None
+
+            else:
+                data[column.name] = value
+
+        return data
+
 
 class TrackedTicker(db.Model):
     __tablename__ = 'tracked_tickers'
